05d27cfbb5958892ad4666912efbbf01fa455801
jai-singh5 <jaisingh09520@gmail.com>
2024-09-30 01:06:25 +0530

    add and fetch notes

--- models/Notes.js
+++ models/Notes.js
@@ -1,7 +1,13 @@
 const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
 
 
 const NotesSchema = new Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user'
+    },
     title: {
         type: String, require: true
     },
--- routes/notes.js
+++ routes/notes.js
@@ -1,9 +1,51 @@
 const express = require('express');
 const router = express.Router();
+var fetchuser = require('../middleware/fetchuser')
+const Notes = require('../models/Notes');
+const { body, validationResult } = require('express-validator');
 
-router.get('/', (req, res)=>{
-   
-    res.json([])
+
+
+
+// ROUTE 1: Get all the Notes using: GET "/api/notes/fetchallnotes". Login required
+router.get('/fetchallnotes', fetchuser, async (req, res) => {
+    try {
+
+        const notes = await Notes.find({ user: req.user.id });
+
+        res.json(notes)
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send("Internal Server Occured");
+
+    }
+})
+
+// ROUTE 2: Add a New Note using: Post "/api/notes/addnote". Login required
+router.post('/addnote', fetchuser, [
+    body('title', 'Title must be atleast 3 character').isLength({ min: 3 }),
+    body('description', 'Description must contain 5 characters').isLength({ min: 5 }),
+], async (req, res) => {
+    try {
+
+        const { title, description, tag } = req.body;
+        // If there are no error return Bad Request and error
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        const note = new Notes({
+            title, description, tag, user: req.user.id
+        })
+        const savedNote = await note.save()
+
+        res.json(savedNote)
+
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send("Internal Server Occured");
+
+    }
 })
 
 module.exports = router 
